c2604859c1870b495052ad821d47126355fad55c
Vizel Leonid <spartanlaska@gmail.com>
2022-06-25 17:47:08 +0300

    Добавьте файлы проекта.

---
+++ ScreenCapturer/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ScreenCapturer
+{
+    public partial class Form1 : Form
+    {
+        private Screen[] screens;
+        public Form1()
+        {
+            screens = Screen.AllScreens;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            workArea.Image?.Dispose();
+            workArea.Image = ScreenCapturer.Capture(screens[displaySwitch.SelectedIndex]);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Screen screen in screens)
+            {
+                displaySwitch.Items.Add(screen.DeviceName);
+            }
+            displaySwitch.SelectedIndex = 0;
+        }
+    }
+}
---
+++ ScreenCapturer/Overlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+//using ZBitmap;
+
+namespace ScreenCapturer
+{
+    public class Overlay
+    {
+        //List<TextOverlay> TextOverlays = new List<TextOverlay>();
+        //List<ImageOverlay> ImageOverlays = new List<ImageOverlay>();
+
+        public void ApplyOverlay(PictureBox source)
+        {
+            Bitmap sourceBitmap = source.Image as Bitmap;
+            //foreach()
+            //{
+
+            //}
+            //Transformations.PasteText(sourceBitmap, )
+        }
+    }
+}
